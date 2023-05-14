c8b4995e1f080885528fc5505eb9b87f11aa6f6e
User <53880692+hhamud@users.noreply.github.com>
2023-05-14 01:25:09 +0100

    feat: update library and schema

--- src/adapter.rs
+++ src/adapter.rs
@@ -1,6 +1,7 @@
 use trustfall::{
     provider::{
-        BasicAdapter, ContextIterator, ContextOutcomeIterator, EdgeParameters, VertexIterator,
+        field_property, resolve_property_with, BasicAdapter, ContextIterator,
+        ContextOutcomeIterator, EdgeParameters, VertexIterator,
     },
     FieldValue, Schema,
 };
@@ -8,12 +9,12 @@ use trustfall::{
 use crate::util::read_file;
 use crate::vertex::Vertex;
 use serde_json;
-use solc_ast::ast::SourceUnit;
+use solc_ast::ast::{NodeType, SourceUnit};
 use std::{error::Error, fs::File, io::BufReader};
 
 #[derive(Debug, Clone, Default)]
 pub struct Soltrust {
-    data: Vec<Vertex>,
+    data: Vec<SourceUnit>,
 }
 
 pub fn read_json(path: &str) -> Result<SourceUnit, Box<dyn Error>> {
@@ -24,10 +25,10 @@ pub fn read_json(path: &str) -> Result<SourceUnit, Box<dyn Error>> {
 
 impl Soltrust {
     pub fn new(path: &str) -> Self {
-        // parse solc ast json file
         let content: SourceUnit = read_json(path).unwrap();
-
-        Self { data: Vec::new() }
+        Self {
+            data: vec![content],
+        }
     }
 
     pub fn schema(&self) -> Schema {
@@ -45,12 +46,18 @@ impl BasicAdapter<'static> for Soltrust {
     ) -> VertexIterator<'static, Self::Vertex> {
         // get data points
         match edge_name.as_ref() {
-            "Contracts" => todo!(),
+            "Contracts" => Box::new(std::iter::once(self.data)),
             "Contract" => {
                 let name = parameters["name"].as_str().unwrap();
-                todo!()
-                //self.data
-                //Box::new(std::iter::once(Vertex::ContractPart())),
+                if let Some(found_contract) = self.data.iter().find(|contract| {
+                    contract
+                        .exported_symbols
+                        .as_ref()
+                        .expect("did not find {name} in the contracts folder")
+                        == name
+                }) {
+                    Box::new(std::iter::once(found_contract))
+                }
             }
             _ => unreachable!("resolve starting vertixes {edge_name}"),
         }
@@ -62,7 +69,38 @@ impl BasicAdapter<'static> for Soltrust {
         type_name: &str,
         property_name: &str,
     ) -> ContextOutcomeIterator<'static, Self::Vertex, FieldValue> {
-        todo!()
+        match type_name {
+            "Pragna" => match property_name {
+                "literals" => resolve_property_with(contexts, field_property!(literals)),
+                "src" => resolve_property_with(contexts, field_property!(src)),
+            },
+            "Contract" => match property_name {
+                "name" => resolve_property_with(contexts, field_property!(name)),
+            },
+            "Import" => match property_name {
+                "file" => resolve_property_with(contexts, field_property!(file)),
+                "unit_alias" => resolve_property_with(contexts, field_property!(unit_alias)),
+            },
+            "FunctionDefinition" => match property_name {
+                "name" => resolve_property_with(contexts, field_property!(name)),
+            },
+            "Parameter" => match property_name {
+                "name" => resolve_property_with(contexts, field_property!(name)),
+                "type" => resolve_property_with(contexts, field_property!(_type)),
+            },
+            "Visibility" => match property_name {
+                "public" => resolve_property_with(contexts, field_property!(public)),
+                "private" => resolve_property_with(contexts, field_property!(private)),
+                "internal" => resolve_property_with(contexts, field_property!(internal)),
+                "external" => resolve_property_with(contexts, field_property!(external)),
+            },
+            "Mutability" => match property_name {
+                "view" => resolve_property_with(contexts, field_property!(view)),
+                "pure" => resolve_property_with(contexts, field_property!(pure)),
+                "payable" => resolve_property_with(contexts, field_property!(payable)),
+                "nonpayable" => resolve_property_with(contexts, field_property!(nonpayable)),
+            },
+        }
     }
 
     fn resolve_neighbors(
@@ -81,14 +119,6 @@ impl BasicAdapter<'static> for Soltrust {
         type_name: &str,
         coerce_to_type: &str,
     ) -> ContextOutcomeIterator<'static, Self::Vertex, bool> {
-        todo!()
-    }
-
-    fn resolve_typename(
-        &mut self,
-        contexts: ContextIterator<'static, Self::Vertex>,
-        _type_name: &str,
-    ) -> ContextOutcomeIterator<'static, Self::Vertex, FieldValue> {
         todo!()
     }
 }
